0f72f145771a5da931e607ab01266f944077c7a4
jas-dev <jaysamora@outlook.com>
2019-03-27 12:54:38 -0700

    completed displaying dummy student data

---
+++ src/dummy_data/student_list.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        name: 'Don',
+        course: 'Advertising',
+        grade: '100',
+        id: 1
+    },
+    {
+        name: 'Dick',
+        course: 'Political Science',
+        grade: '100',
+        id: 2
+    },
+    {
+        name: 'Darrell',
+        course: 'Logistics',
+        grade: '100',
+        id: 3
+    },
+    {
+        name: 'Dan',
+        course: 'Computer Science',
+        grade: '100',
+        id: 4
+    },
+    {
+        name: 'Dennis',
+        course: 'Art',
+        grade: '100',
+        id: 5
+    }
+];
