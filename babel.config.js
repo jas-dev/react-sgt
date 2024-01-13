923100cff757f2ddbdc13823410eb18892f071a3
jas-dev <jaysamora@outlook.com>
2024-01-13 10:05:57 -0700

    updated babel config to integrate react-refresh

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
 module.exports = function (api) {
   api.cache.forever();
+  
+  const isDevelopment = process.env.NODE_ENV !== 'production';
 
   return {
     presets: [
@@ -7,7 +9,7 @@ module.exports = function (api) {
         "@babel/preset-env",
         {
           useBuiltIns: "entry",
-          corejs: 3 // Specify the version of core-js
+          corejs: 3
         }
       ],
       "@babel/preset-react"
@@ -15,20 +17,21 @@ module.exports = function (api) {
     plugins: [
       "syntax-async-functions",
       ["@babel/plugin-proposal-decorators", { "legacy": true }],
-      "@babel/plugin-transform-export-namespace-from", // Updated
+      "@babel/plugin-transform-export-namespace-from",
       "@babel/plugin-proposal-function-sent",
-      "@babel/plugin-transform-json-strings", // Updated
-      "@babel/plugin-transform-numeric-separator", // Updated
-      "@babel/plugin-transform-optional-chaining", // Updated
+      "@babel/plugin-transform-json-strings",
+      "@babel/plugin-transform-numeric-separator",
+      "@babel/plugin-transform-optional-chaining",
       "@babel/plugin-proposal-throw-expressions",
       "@babel/plugin-syntax-dynamic-import",
       "@babel/plugin-syntax-import-meta",
       [
-        "@babel/plugin-transform-class-properties", // Updated
+        "@babel/plugin-transform-class-properties",
         {
           "loose": true
         }
-      ]
-    ]
+      ],
+      isDevelopment && require.resolve('react-refresh/babel')
+    ].filter(Boolean) // Filter out false values in development mode
   }
 }
