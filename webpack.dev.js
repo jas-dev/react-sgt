8ce3641ded6ac03cda145cafabb7c9b049d1d7e5
jas-dev <jaysamora@outlook.com>
2024-01-13 11:23:41 -0700

    refactored webpack dev server to remove babel polyfill entry and add react-refresh configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,21 +1,40 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const isDevelopment = process.env.NODE_ENV !== 'production';
 const common = require('./webpack.common');
 const PORT = process.env.PORT || 3000;
 
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 module.exports = merge(common, {
     entry: [
-        '@babel/polyfill',
         'webpack-dev-server/client?http://localhost:' + PORT,
         './index.js'
     ],
     devtool: 'inline-source-map',
-    mode: isDevelopment ? 'development' : 'production',
+    mode: 'development', 
     plugins: [
-        new webpack.NamedModulesPlugin()
-    ],
+        new webpack.NamedModulesPlugin(),
+        isDevelopment && new ReactRefreshWebpackPlugin() // Add the React Refresh Plugin conditionally
+    ].filter(Boolean), // Filter to remove false values in case of production mode
     devServer: {
-        hot: true,
+        hot: true, // Enable Hot Module Replacement
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: require.resolve('babel-loader'),
+                        options: {
+                            // This part is handled in the Babel configuration file
+                            // plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
+                        },
+                    },
+                ],
+            },
+        ],
     }
 });
